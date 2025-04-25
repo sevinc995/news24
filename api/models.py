2a8a9c9643caa4b49cd07aae83e1e36598e0fd29
sevinc995 <sevincseferova25@gmail.com>
2025-04-25 19:13:59 +0000

    models

--- api/models.py
+++ api/models.py
@@ -19,11 +19,112 @@ news2 = News(
 )
 
 news3 = News(
-    "Meta unveils AI video tool: Movie Gen".
+    "Meta unveils AI video tool: Movie Gen",
     "Movie Gen can create new clips from text prompts or edit your face into existing footage - complete with sound.",
     "https://picsum.photos/400?random=3"
 )
 
+news4 = News(
+    "Microsoft made an ad with generative AI and nobody noticed",
+    "The ninute-long ad has been live for months, but Microsoft only just explained AI's involvement.",
+    "https://picsum.photos/400?random=4"
+)
+
+news5 = News(
+    "Windows 11’s voice typing will soon let you turn off the ****ing profanity filter",
+    "Ideal for everyone who likes to throw a few swear words into their sentences.",
+    "https://picsum.photos/400?random=5"
+)
+
+news6 = News(
+    "Google is killing software support for early Nest Thermostats",
+    "The company is also pulling Nest thermostats out of Europe entirely, citing “unique” heating challenges.",
+    "https://picsum.photos/400?random=6"
+)
+
+news7 = News(
+    "Microsoft launches Recall and AI-powered Windows search for Copilot Plus PCs",
+    "Microsoft’s Click to Do image and text tool is also rolling out today.",
+    "https://picsum.photos/400?random=7"
+)
+
+news8 = News(
+    "Adidas’ 3D-printed shoes are launching globally on May 2nd",
+    "Originally only available in limited quantities, the Climacool will be available to everyone next week for $140.",
+    "https://picsum.photos/400?random=8"
+)
+
+news9 = News(
+    "Bill Gates’ daughter Phoebe launched a shopping app",
+    "The app, called Phia, will compare the price of clothing, shoes, and accessories with new and used listings.",
+    "https://picsum.photos/400?random=9"
+)
+
+news10 = News(
+    "Waymo might be willing to sell you a self-driving car, says Sundar Pichai",
+    "The Alphabet CEO said “personal ownership” of Waymo’s lidar-equipped cars is an option long-term.",
+    "https://picsum.photos/400?random=10"
+)
+
+news11 = News(
+    "An AI-generated radio host in Australia went unnoticed for months",
+    "The show, called Workdays with Thy, uses the AI-generated voice and likeness of a real person during a four-hour segment.",
+    "https://picsum.photos/400?random=11"
+)
+
+news12 = News(
+    "Boox’s newest Android e-readers let you take notes",
+    "The 7-inch e-readers are available with full color or black-and-white E Ink screens and an optional stylus.",
+    "https://picsum.photos/400?random=12"
+)
+
+news13 = News(
+    "The world’s biggest zipper maker is developing a self-propelled zipper",
+    "YKK’s tent-sized prototype can zip itself up at the push of a button.",
+    "https://picsum.photos/400?random=13"
+)
+
+news14 = News(
+    "ChatGPT is getting a ‘lightweight’ version of its deep research tool",
+    "It’s the first version of deep research available to ChatGPT’s free users.",
+    "https://picsum.photos/400?random=14"
+)
+
+news15 = News(
+    "Apple Recommends Blinkist, The Best App For Intellectuals Around The World",
+    "Apple recommends Blinkist for lifelong learners, top thinkers, and anyone who wishes they had more time to learn the powerful ideas in nonfiction books.",
+    "https://picsum.photos/400?random=15"
+)
+
+news16 = News(
+    "Microsoft’s design chief on human creation in the AI era",
+    "As Microsoft focuses on agents, designers’ jobs are changing in the AI era.",
+    "https://picsum.photos/400?random=16"
+)
+
+news17 = News(
+    "Figma is working on an AI app maker.",
+    "That’s according to security researcher Jane Manchun Wong, who writes that the feature “accepts text prompt, Figma files, images, etc. as input, and is powered by Anthropic’s Claude Sonnet model.",
+    "https://picsum.photos/400?random=17"
+)
+
+news18 = News(
+    "BBC Sport extends London Marathon deal to 2030",
+    "BBC Sport has agreed a new contract to broadcast the London Marathon to 2030 - the year of the iconic race's 50th edition.",
+    "https://picsum.photos/400?random=18"
+)
+
+news19 = News(
+    "Once a fool to become an expert' - Yee set for marathon debut",
+    "Alex Yee is making his London Marathon debut 10 years after completing the Mini London Marathon for a fifth time",
+    "https://picsum.photos/400?random=19"
+)
+
+news20 = News(
+    "London Marathon 2025 - everything you need to know",
+    "Organisers hope the 45th edition of the London Marathon will set a world record",
+    "https://picsum.photos/400?random=20"
+)
 
 
 news_list = []
