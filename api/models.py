11891f6208ecb79a54d4d1b53df0a0ef730a7085
sevinc995 <sevincseferova25@gmail.com>
2025-04-25 13:09:31 +0000

    news3

--- api/models.py
+++ api/models.py
@@ -7,9 +7,23 @@ class News:
         self.img = img
 
 news1 = News(
-    "iPhone 16 Pro Max",
-    "The iPhone 16 Pro Max will feature a larger 6.9-inch display, powered by the A18 Pro chip with enhanted AI capabilities, end include a new dedicated Capture button for improved camera  functionality.",
-    "https://raw.githubusercontent.com/sevinc995/imagess/refs/heads/main/iphone.webp"
+    "Ray-Ban Meta",
+    "You can now use the glasses to remind you make a phone call or even where you parked your car."
+    "https://picsum.photos/400?random=1"
 )
 
-news_list = [news1]
+news2 = News(
+    "Amazon's new Fire tablet",
+    "The company's refreshed Fire HD 8 will get some new AI tools, but they're rolling out to other Amazon tablets, too.",
+    "https://picsum.photos/400?random=2"
+)
+
+news3 = News(
+    "Meta unveils AI video tool: Movie Gen".
+    "Movie Gen can create new clips from text prompts or edit your face into existing footage - complete with sound.",
+    "https://picsum.photos/400?random=3"
+)
+
+
+
+news_list = []
